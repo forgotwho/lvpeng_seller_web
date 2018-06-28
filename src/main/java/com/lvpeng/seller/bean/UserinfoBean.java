007041b4f5c19466c856ed2b573bca7529588e7d
zhangxuesheng <zhangxuesheng@forte.com.cn>
2018-06-28 17:20:18 +0800

    bean

---
+++ src/main/java/com/lvpeng/seller/bean/UserinfoBean.java
@@ -0,0 +1,26 @@
+/**
+  * Copyright 2018 bejson.com 
+  */
+package com.lvpeng.seller.bean;
+
+import com.lvpeng.seller.dal.model.Customer;
+
+/**
+ * Auto-generated: 2018-06-23 23:6:4
+ *
+ * @author bejson.com (i@bejson.com)
+ * @website http://www.bejson.com/java2pojo/
+ */
+public class UserinfoBean {
+
+	private Customer user;
+
+	public void setUser(Customer user) {
+		this.user = user;
+	}
+
+	public Customer getUser() {
+		return user;
+	}
+
+}
